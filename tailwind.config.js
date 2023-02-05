d49020f9bf9dc36aa05a73fd6035b62c59f3a060
Daniel Villegas <avill.daniel@gmail.com>
2023-02-05 11:27:43 -0800

    feat: custom fonts & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "ibm-light": "IBM Plex Sans Light",
+        "ibm-semi": "IBM Plex Sans Semi",
+        goth: "League Gothic",
+      },
+    },
+    colors: {
+      "gray-200": "#8F8F8F",
+      "gray-500": "#AEAEAE",
+      "gray-800": "#191919",
+      yellow: "#FFE600",
+      brick: "#B93333",
+      "brick-300": "#612323",
+      black: "#000",
+      white: "#FFF",
+    },
   },
   plugins: [],
 };
