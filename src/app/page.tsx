f69221ea8a5927eddfebcc885384b268bf2f609e
Daniel Villegas <avill.daniel@gmail.com>
2023-02-12 14:08:58 -0800

    feat: randomize group

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,16 @@
 "use client";
 
-import {
-  Dispatch,
-  ReactNode,
-  SetStateAction,
-  useEffect,
-  useState,
-} from "react";
-import Select, {
-  DropdownIndicatorProps,
-  components,
-  SingleValue,
-  GroupBase,
-} from "react-select";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+// import Select, {
+//   DropdownIndicatorProps,
+//   components,
+//   SingleValue,
+//   GroupBase,
+// } from "react-select";
 // import data from "../champs.json";
 import data from "../champsV2.json";
-import { TiArrowSortedDown } from "react-icons/ti";
+export type DataType = typeof data;
+import { TiArrowLeft } from "react-icons/ti";
 import { MdClose } from "react-icons/md";
 import Image from "next/image";
 
@@ -74,88 +69,129 @@ const GrpSection = ({
 );
 
 function App() {
-  const [group, setGroup] = useState<
-    SingleValue<{ value: string; label: string }>
-  >(OPTIONS[0]);
+  // const [group, setGroup] = useState<
+  //   SingleValue<{ value: string; label: string }>
+  // >(OPTIONS[0]);
 
   const { assassins, fighters, mages, marksmen, supports, tanks } = data;
   const [selected, setSelected] = useState({ category: "", champ: "" });
-  const [champs, setChamps] = useState(() => {
-    return assassins.champs.map((champ) => ({
-      ...champ,
-      selected: false,
-    }));
-  });
+  // const [champs, setChamps] = useState(() => {
+  //   return assassins.champs.map((champ) => ({
+  //     ...champ,
+  //     selected: false,
+  //   }));
+  // });
+  const [champs, setChamps] = useState<
+    {
+      selected: boolean;
+      name: string;
+      img: string;
+    }[]
+  >([]);
+
+  // useEffect(() => {
+  //   switch (group?.value) {
+  //     case "assassins":
+  //       setChamps(() => {
+  //         return assassins.champs.map((champ) => ({
+  //           ...champ,
+  //           selected: false,
+  //           // selected: true,
+  //         }));
+  //       });
+  //       break;
+  //     case "fighters":
+  //       setChamps(() => {
+  //         return fighters.champs.map((champ) => ({
+  //           ...champ,
+  //           selected: false,
+  //         }));
+  //       });
+  //       break;
+  //     case "mages":
+  //       setChamps(() => {
+  //         return mages.champs.map((champ) => ({
+  //           ...champ,
+  //           selected: false,
+  //         }));
+  //       });
+  //       break;
+  //     case "marksmen":
+  //       setChamps(() => {
+  //         return marksmen.champs.map((champ) => ({
+  //           ...champ,
+  //           selected: false,
+  //         }));
+  //       });
+  //       break;
+  //     case "supports":
+  //       setChamps(() => {
+  //         return supports.champs.map((champ) => ({
+  //           ...champ,
+  //           selected: false,
+  //         }));
+  //       });
+  //       break;
+  //     case "tanks":
+  //       setChamps(() => {
+  //         return tanks.champs.map((champ) => ({
+  //           ...champ,
+  //           selected: false,
+  //         }));
+  //       });
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }, [group]);
+
+  const setChampFn = (selected: { category: string; champ: string }) => {
+    setChamps(() => {
+      const champJson = data[selected.category as keyof DataType].champs.find(
+        (champ) => champ.name === selected.champ
+      );
+
+      const arr = data[selected.category as keyof DataType].champs
+        .map((champ) => ({
+          ...champ,
+          selected: false,
+        }))
+        .slice(0, 30);
+
+      const found = arr.find((champ) => champ.name === selected.champ);
+      if (!found && champJson) {
+        arr[0] = { ...champJson, selected: false };
+      }
+
+      var j, x, index;
+      for (index = arr.length - 1; index > 0; index--) {
+        j = Math.floor(Math.random() * (index + 1));
+        x = arr[index];
+        arr[index] = arr[j];
+        arr[j] = x;
+      }
+
+      return arr;
+    });
+  };
 
   useEffect(() => {
-    switch (group?.value) {
-      case "assassins":
-        setChamps(() => {
-          return assassins.champs.map((champ) => ({
-            ...champ,
-            selected: false,
-            // selected: true,
-          }));
-        });
-        break;
-      case "fighters":
-        setChamps(() => {
-          return fighters.champs.map((champ) => ({
-            ...champ,
-            selected: false,
-          }));
-        });
-        break;
-      case "mages":
-        setChamps(() => {
-          return mages.champs.map((champ) => ({
-            ...champ,
-            selected: false,
-          }));
-        });
-        break;
-      case "marksmen":
-        setChamps(() => {
-          return marksmen.champs.map((champ) => ({
-            ...champ,
-            selected: false,
-          }));
-        });
-        break;
-      case "supports":
-        setChamps(() => {
-          return supports.champs.map((champ) => ({
-            ...champ,
-            selected: false,
-          }));
-        });
-        break;
-      case "tanks":
-        setChamps(() => {
-          return tanks.champs.map((champ) => ({
-            ...champ,
-            selected: false,
-          }));
-        });
-        break;
-      default:
-        break;
-    }
-  }, [group]);
+    if (selected.champ) setChampFn(selected);
+  }, [selected]);
 
-  const DropdownIndicator = (
-    props: DropdownIndicatorProps<
-      { value: string; label: string },
-      boolean,
-      GroupBase<{ value: string; label: string }>
-    >
-  ) => {
-    return (
-      <components.DropdownIndicator {...props} className="zeroPad">
-        <TiArrowSortedDown className="text-gray-500" size={32} />
-      </components.DropdownIndicator>
-    );
-  };
+  // const DropdownIndicator = (
+  //   props: DropdownIndicatorProps<
+  //     { value: string; label: string },
+  //     boolean,
+  //     GroupBase<{ value: string; label: string }>
+  //   >
+  // ) => {
+  //   return (
+  //     <components.DropdownIndicator {...props} className="zeroPad">
+  //       <TiArrowSortedDown className="text-gray-500" size={32} />
+  //     </components.DropdownIndicator>
+  //   );
+  // };
 
   if (!selected.champ) {
     return (
@@ -165,7 +201,7 @@ function App() {
           champs={data.assassins.champs}
           setSelected={setSelected}
           // className="w-1/6"
-          className="w-[16%]"
+          className="w-[15%]"
         />
         <GrpSection
           cat="FIGHTERS"
@@ -177,7 +213,7 @@ function App() {
           cat="MAGES"
           champs={data.mages.champs}
           setSelected={setSelected}
-          className="w-[17%]"
+          className="w-[18%]"
         />
         <GrpSection
           cat="MARKSMEN"
@@ -205,7 +241,7 @@ function App() {
   }
 
   return (
-    <div className="h-screen w-full flex flex-col justify-evenly items-center">
+    <div className="h-screen w-full flex flex-col justify-evenly items-center relative">
       {/* Select Dropdown */}
       {/* <div className="w-72 self-start -mt-10 ml-28">
         <Select
@@ -270,8 +306,11 @@ function App() {
         />
       </div> */}
 
-      <button onClick={() => setSelected({ category: "", champ: "" })}>
-        {"<-"} Go Back
+      <button
+        className="absolute top-2 left-2 font-ibm-semi text-3xl flex items-center gap-x-2"
+        onClick={() => setSelected({ category: "", champ: "" })}
+      >
+        <TiArrowLeft size={40} /> Go Back
       </button>
 
       {/* Champ Grid */}
